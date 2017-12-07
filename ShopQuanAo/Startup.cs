f0c55bf8f5b44f6ffb397fcfbe2bff39b79fd70c
Anh Nguyen Dinh <nd.anh@hutech.edu.vn>
2017-12-07 07:46:46 +0700

    Add project files.

---
+++ ShopQuanAo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopQuanAo.Startup))]
+namespace ShopQuanAo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
