232261a30c1d4a3b6ce6bfc49d1869a41c8e72c0
Anh Nguyen <nd.anh@hutech.edu.vn>
2017-12-23 13:47:44 +0700

    Delete product Ajax

--- ShopQuanAo/Controllers/Apis/SanPhamsController.cs
+++ ShopQuanAo/Controllers/Apis/SanPhamsController.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using ShopQuanAo.DataContext;
 using System.Web.Http;
-using System.Web.Http.Description;
-using ShopQuanAo.DataContext;
 
 namespace ShopQuanAo.Controllers.Apis
 {
@@ -16,99 +7,18 @@ namespace ShopQuanAo.Controllers.Apis
     {
         private ShopQuanAoEntities db = new ShopQuanAoEntities();
 
-        // GET: api/SanPhams
-        public IQueryable<SanPham> GetSanPhams()
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
         {
-            return db.SanPhams;
-        }
+            var product = db.SanPhams.Find(id);
 
-        // GET: api/SanPhams/5
-        public IHttpActionResult GetSanPham(int id)
-        {
-            SanPham sanPham = db.SanPhams.Find(id);
-            if (sanPham == null)
-            {
+            if (product == null)
                 return NotFound();
-            }
 
-            return Ok(sanPham);
-        }
-
-        // PUT: api/SanPhams/5
-        public IHttpActionResult PutSanPham(int id, SanPham sanPham)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != sanPham.MaSP)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(sanPham).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SanPhamExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/SanPhams
-        public IHttpActionResult PostSanPham(SanPham sanPham)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.SanPhams.Add(sanPham);
+            db.SanPhams.Remove(product);
             db.SaveChanges();
 
-            return CreatedAtRoute("DefaultApi", new { id = sanPham.MaSP }, sanPham);
-        }
-
-        // DELETE: api/SanPhams/5
-        public IHttpActionResult DeleteSanPham(int id)
-        {
-            SanPham sanPham = db.SanPhams.Find(id);
-            if (sanPham == null)
-            {
-                return NotFound();
-            }
-
-            db.SanPhams.Remove(sanPham);
-            db.SaveChanges();
-
-            return Ok(sanPham);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool SanPhamExists(int id)
-        {
-            return db.SanPhams.Count(e => e.MaSP == id) > 0;
+            return Ok();
         }
     }
 }
--- ShopQuanAo/Global.asax.cs
+++ ShopQuanAo/Global.asax.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
-using System.Web.Http;
 using System.Web.Routing;
 
 namespace ShopQuanAo
@@ -14,19 +9,11 @@ namespace ShopQuanAo
     {
         protected void Application_Start()
         {
-
-
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-
-
-            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
-            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.All;
-
         }
     }
 }
